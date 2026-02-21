69d4e2a85d11a45b5db5e92acb622d8d10d7669f
Ami Pedahzur <8159264+pedahzur@users.noreply.github.com>
2026-02-21 08:40:01 +0200

    Update publication entries in script.js

--- script.js
+++ script.js
@@ -1,42 +1,66 @@
 const publications = [
   {
-    title: "From Captive to Catalyst: The Militant Utility of Female Childbearing During Captivity",
-    authors: "Yochim Berger and Ami Pedahzur",
-    venue: "Studies in Conflict & Terrorism, 2022",
+    title: "Territorial Control and the Militarization of Violent Non-State Actors: Transformation, Opportunities, and Vulnerabilities",
+    authors: "Ofir Dayan and Ami Pedahzur",
+    venue: "Studies in Conflict and Terrorism, 2025",
     links: [
-      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=From+Captive+to+Catalyst+The+Militant+Utility+of+Female+Childbearing+During+Captivity" }
+      { label: "DOI", url: "https://doi.org/10.1080/1057610X.2025.2528333" }
     ]
   },
   {
-    title: "The Human-Machine Team: How New Technologies Shape the Contemporary Battlefield",
-    authors: "Todd M. Sweeney, Yochim Berger, and Ami Pedahzur",
-    venue: "Armed Forces & Society, 2021",
+    title: "The Munich Massacre and the Proliferation of Counter-Terrorism Special Operation Forces",
+    authors: "Ronit Berger and Ami Pedahzur",
+    venue: "Israel Affairs, Vol. 28, No. 4, Pp. 625–637, 2022",
     links: [
-      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=The+Human-Machine+Team+How+New+Technologies+Shape+the+Contemporary+Battlefield" }
+      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=The+Munich+Massacre+and+the+Proliferation+of+Counter-Terrorism+Special+Operation+Forces+Pedahzur" }
     ]
   },
   {
-    title: "The Posse and the Revolving Door: A New Dataset on Israeli Special Operations Units and Their Veterans in Politics",
-    authors: "Guy D. Grossman and Ami Pedahzur",
-    venue: "Terrorism and Political Violence, 2021",
+    title: "Reconstructing the Theater of Terror",
+    authors: "Matthew Sweeney, Arie Perliger, and Ami Pedahzur",
+    venue: "Small Wars & Insurgencies, Vol. 32, No. 3, Pp. 469–489, 2021",
     links: [
-      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=The+Posse+and+the+Revolving+Door+A+New+Dataset+on+Israeli+Special+Operations+Units+and+Their+Veterans+in+Politics" }
+      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=Reconstructing+the+Theater+of+Terror+Sweeney+Perliger+Pedahzur" }
     ]
   },
   {
-    title: "Civil-Military Relations and Regime Stability in Arab Dictatorships",
-    authors: "Guy D. Grossman and Ami Pedahzur",
-    venue: "International Journal of Comparative Sociology, 2020",
+    title: "Can We Do Better? Replication and Online Appendices in Political Science",
+    authors: "Jonathan Grossman and Ami Pedahzur",
+    venue: "Perspectives on Politics, Vol. 19, No. 3, Pp. 906–911, 2021",
     links: [
-      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=Civil-Military+Relations+and+Regime+Stability+in+Arab+Dictatorships" }
+      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=Can+We+Do+Better+Replication+Online+Appendices+Political+Science+Grossman+Pedahzur" }
     ]
   },
   {
-    title: "The Triumph of Israel's Radical Right",
-    authors: "Ami Pedahzur",
-    venue: "Oxford University Press, 2012",
+    title: "Political Science and Big Data: Why We Need Unstructured Big Data and How to Use It",
+    authors: "Jonathan Grossman and Ami Pedahzur",
+    venue: "Political Science Quarterly, Vol. 135, No. 2, Pp. 225–257, 2020",
     links: [
-      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=The+Triumph+of+Israel%27s+Radical+Right+Ami+Pedahzur" }
+      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=Political+Science+Big+Data+Unstructured+Grossman+Pedahzur" }
+    ]
+  },
+  {
+    title: "Counter-Cultures, Group Dynamics and Religious Terrorism",
+    authors: "Arie Perliger and Ami Pedahzur",
+    venue: "Political Studies, Vol. 64, No. 2, Pp. 297–314, 2016",
+    links: [
+      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=Counter-Cultures+Group+Dynamics+Religious+Terrorism+Perliger+Pedahzur" }
+    ]
+  },
+  {
+    title: "Social Network Analysis in the Study of Terrorism and Political Violence",
+    authors: "Arie Perliger and Ami Pedahzur",
+    venue: "PS: Political Science and Politics, Vol. 44, No. 1, Pp. 45–50, 2011",
+    links: [
+      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=Social+Network+Analysis+Study+Terrorism+Political+Violence+Perliger+Pedahzur" }
+    ]
+  },
+  {
+    title: "The Changing Nature of Suicide Attacks: A Social Network Perspective",
+    authors: "Ami Pedahzur and Arie Perliger",
+    venue: "Social Forces, Vol. 84, No. 4, Pp. 1983–2004, 2006",
+    links: [
+      { label: "Google Scholar", url: "https://scholar.google.com/scholar?q=The+Changing+Nature+of+Suicide+Attacks+Social+Network+Perspective+Pedahzur+Perliger" }
     ]
   }
 ];
